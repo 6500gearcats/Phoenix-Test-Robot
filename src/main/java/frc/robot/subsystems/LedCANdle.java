79f068fc0e2a1d34248e5d24bef23f39e85537a0
josephsilber-jpg <josephsilber123@gmail.com>
2025-12-16 23:21:44 -0500

    forgot to stage changes from the last commit

---
+++ src/main/java/frc/robot/subsystems/LedCANdle.java
@@ -0,0 +1,70 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+import com.ctre.phoenix6.configs.CANdleConfiguration;
+import com.ctre.phoenix6.controls.EmptyAnimation;
+import com.ctre.phoenix6.controls.RainbowAnimation;
+import com.ctre.phoenix6.hardware.CANdle;
+import com.ctre.phoenix6.controls.SolidColor;
+import com.ctre.phoenix6.signals.RGBWColor;
+
+import edu.wpi.first.wpilibj.Timer;
+
+public class LedCANdle extends SubsystemBase {
+  private static CANdle candle = new CANdle(99); //TODO: change id
+  private static CANdleConfiguration config = new CANdleConfiguration();
+  private static EmptyAnimation empty = new EmptyAnimation(0);
+  private static RainbowAnimation rainbow = new RainbowAnimation(0, 7);
+
+  private static Timer timer = new Timer();
+  private static int fps = 20;
+
+  private final CommandXboxController joystick = new CommandXboxController(0);
+  private static boolean joystickView = false;
+
+  /** Creates a new CANdle. */
+  public LedCANdle() {
+    candle.getConfigurator().apply(config);
+    candle.setControl(new SolidColor(0, 7).withColor(new RGBWColor(0, 255, 0)));
+
+    timer.restart();
+  }
+
+  public void setLedColor(int r, int g, int b) {
+    candle.setControl(new SolidColor(0, 7).withColor(new RGBWColor(r, g, b)));
+    joystickView = false;
+  }
+
+  public void setRainbowAnimation() {
+    candle.setControl(rainbow);
+    joystickView = true;
+  }
+
+  public void enableJoystickView() {
+    if(!joystickView) {
+      joystickView = true;
+      candle.setControl(empty);
+      timer.restart();
+    }
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    if((joystickView) && (timer.get() >= (1.0 / fps))) {
+      double whiteFactor = ((joystick.getLeftX() * -1) + 1) / 2;
+      candle.setControl(new SolidColor(0, 7)
+        .withColor(new RGBWColor(255, 255, 255, (int)(whiteFactor * 255))));
+
+      timer.restart();
+    }
+  }
+}
